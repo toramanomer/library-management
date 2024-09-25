1639d910bf32159990d480c9706bab0bef4249c6
Ömer Toraman <toramanomer@icloud.com>
2024-09-25 18:23:04 +0300

    install eslint, typescript, tsx

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  { rules: {
+	"indent": ["error", "tab"],
+	"quotes": ["error", "single"],
+	"semi": ["error", "never"],
+	"object-curly-newline": ["error", { "multiline": true }],
+	"no-duplicate-imports": "error",
+	"no-extra-parens": "error",
+  }}
+];
