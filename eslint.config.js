722e6105e4d853c46355cb8291f47aacc94fa549
Ömer Toraman <toramanomer@icloud.com>
2024-09-25 20:32:58 +0300

    Update eslint.config.js with consistent code formatting rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,24 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  { rules: {
-	"indent": ["error", "tab"],
-	"quotes": ["error", "single"],
-	"semi": ["error", "never"],
-	"object-curly-newline": ["error", { "multiline": true }],
-	"no-duplicate-imports": "error",
-	"no-extra-parens": "error",
-  }}
-];
+	{ files: ['**/*.{js,mjs,cjs,ts}'] },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		rules: {
+			'indent': ['error', 'tab'],
+			'quotes': ['error', 'single'],
+			'semi': ['error', 'never'],
+			'object-curly-newline': ['error', { 'multiline': true }],
+			'no-duplicate-imports': 'error',
+			'no-extra-parens': 'error',
+			'no-trailing-spaces': 'error',
+			'object-curly-spacing': ['error', 'always'],
+			'comma-spacing': ['error', { 'before': false, 'after': true }],
+		}
+	}
+]
