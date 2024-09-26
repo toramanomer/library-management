fa7c491383096689d6fe0777858606039f020e6a
Ömer Toraman <toramanomer@icloud.com>
2024-09-26 08:35:32 +0300

    Add user routes and handlers for creating, listing, and retrieving users

---
+++ src/routes/users.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import { processRequestBody, processRequestParams } from 'zod-express-middleware'
+
+import { createUser, createUserSchema } from '../handlers/users/create-user'
+import { listUsers } from '../handlers/users/list-users'
+import { getUser, getUserSchema } from '../handlers/users/get-user'
+import { borrowBook, borrowBookSchema } from '../handlers/users/borrow-book'
+import { returnBook } from '../handlers/users/return-book'
+
+export const userRouter = Router()
+
+userRouter.use('/:userId/borrow/:bookId', processRequestParams(borrowBookSchema), borrowBook)
+userRouter.use('/users/:userId/return/:bookId', returnBook)
+userRouter.get('/:id', processRequestParams(getUserSchema), getUser)
+userRouter.post('/', processRequestBody(createUserSchema), createUser)
+userRouter.get('/', listUsers)
